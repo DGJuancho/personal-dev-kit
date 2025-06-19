e6e43f322bd4aeb3f99191290dd5546ec818611f
DGJuancho <juancho.designs@icloud.com>
2025-06-19 19:14:21 -0400

    chore🔧(alias): añade alias para staging selectivo, diff visual y log detallado

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,6 +1,21 @@
-# Alias útiles
-alias gs='git status'
-alias ga='git add .'
-alias gc='git commit -m'
-alias gp='git push'
-alias gpl='git pull'
+# ==== Alias útiles de Git ====
+alias gs='git status'                   # Ver estado del repositorio
+alias ga='git add .'                   # Añadir todos los archivos modificados
+alias gai='git add -i'                # Añadir archivos de forma interactiva (por número)
+alias gap='git add -p'                # Añadir fragmentos de código (por bloque/hunk)
+alias gdiff='git diff --color'        # Ver diferencias con resaltado antes del stage
+alias gc='git commit -m'              # Commit con mensaje directo
+alias gp='git push'                   # Enviar commits al repositorio remoto
+alias gpl='git pull'                  # Traer actualizaciones del repositorio remoto
+alias gl='git log --oneline --graph --decorate'  # Historial compacto y visual
+alias gll='git log --graph --pretty=format:"%C(yellow)%h%Creset %Cgreen%ad%Creset %C(blue)%an%Creset %C(auto)%d%Creset %s" --date=short'    # visualización ampliada más detalles (autor, fecha, mensaje)ls
+
+
+# ==== Alias de sistema ====
+alias ll='ls -l'                                      # Listado largo de archivos
+alias ls='ls -F --color=auto --show-control-chars'    # Listado con formato visual
+
+# ==== Ejecutables Windows bajo MSYS/Git Bash ====
+alias node='winpty node.exe'
+alias php='winpty php.exe'
+alias winget='winpty winget.exe'
